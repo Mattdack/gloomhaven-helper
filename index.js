8677e59cee0a2ca87989dbb4b14008b0d441f0cc
dthiel22 <dthiel22@gmail.com>
2022-11-07 14:15:17 -0800

    changing routes folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-// const routes = require('./controllers');
+const routes = require('./controllers');
 // const sequelize = require('./config/connection');
 
 const app = express();

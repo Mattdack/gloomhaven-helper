5ae3632b10eea4c3b614fa7165a40c263208ed35
Dian Sherman <slayer_barrett_@hotmail.com>
2022-11-07 13:56:47 -0800

    turn order work

--- controllers/api/turnorderController.js
+++ controllers/api/turnorderController.js
@@ -1,37 +1,64 @@
 const router = require('express').Router();
-const { Campaign } = require('../../models');
+const { Campaign, Player, Monster } = require('../../models');
 
-router.post('/', async (req, res) => {
-  try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+router.get('/'), async (req, res) => {
+    try {
+        const dbCharacterData = await Campaign.findAll({
+            include: [
+                {
+                    model: Player,
+                    attributes: ['playerName']
+                },
+                {
+                    model: Monster,
+                    attributes: ["name"]
+                }
+            ]
+        });
 
-    res.status(200).json(newProject);
-  } catch (err) {
+        const newList = dbCharacterData.map((characters) =>
+        characters.get({ plain: true })
+        );
+
+        res.render('Turn Order', {
+            characters,
+        })
+    } catch (err) {
     res.status(400).json(err);
   }
-});
+}
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+// router.post('/', async (req, res) => {
+//   try {
+//     const newCampaignList = await Campaign.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
-    }
+//     res.status(200).json(newCampaign);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
-    res.status(200).json(projectData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.delete('/', async (req, res) => {
+//   try {
+//     const campaignData = await .destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!campaignData) {
+//       res.status(404).json({ message: 'No campaign found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(campaignData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;

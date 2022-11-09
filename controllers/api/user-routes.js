385d85068f132899b3a11335148e027666ff164b
dthiel22 <dthiel22@gmail.com>
2022-11-08 18:03:37 -0800

    login feature added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -74,4 +74,39 @@ router.delete('/:id', (req, res) => {
     .catch((err) => res.json(err));
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        // TODO: add in username AND password
+        .json({ message: 'Incorrect username, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        // TODO: add in username AND password
+        .json({ message: 'Incorrect password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ username: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt');
 const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -11,7 +16,6 @@ User.init(
       unique: true,
       validate: {
         len: [8, 30],
-        isAlphaNumeric: true,
       },
     },
     password: {
@@ -23,7 +27,18 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 3);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 3);
+        return updatedUserData;
+      },
+    },
     sequelize,
+    modelName: 'user',
   }
 );
 
